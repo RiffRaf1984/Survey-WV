279edf0a63db80c42e26c185964753f07ac443f5
patrickmckowen <patrickmckowen1@gmail.com>
2018-04-10 23:01:16 -0400

    Perhaps finished adapting structure for heroku. About to test now.

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from . import app
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/survey")
+def survey():
+    return render_template('survey.html')
