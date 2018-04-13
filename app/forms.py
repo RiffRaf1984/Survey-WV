1824ed2a11dade18ead35c549de9ef928c73458d
patrickmckowen <patrickmckowen1@gmail.com>
2018-04-13 17:38:09 -0400

    Added manually typed html form.

--- app/forms.py
+++ app/forms.py
@@ -2,81 +2,87 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, RadioField, DecimalField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length
 
-
 event_list = [
-    ("Art Fair", ''), 
-    ("Air Play", ''),
-    ("Art Parade", ''),
-    ("ArtWalk", ''),
-    ("Cabaret FestivAll", ''),
-    ("Carriage Trail", ''),
-    ("Chili Cookoff", ''),
-    ("Civic Chorus", ''),
-    ("CLOG", ''),
-    ("CYAC", ''),
-    ("Dance Gala", ''),
-    ("Drum Circle", ''),
-    ("Festiv-ALT", ''),
-    ("Go to Hale", ''),
-    ("Ice Cream Social", ''),
-    ("Juneteenth", ''),
-    ("Live on the Levee", ''),
-    ("Mayor's Concert", ''),
-    ("Mountain Stage", ''),
-    ("Taste", ''),
-    ("Wine & All That Jazz", ''),
-    ("Woofstock & Wieners", ''),
-    ("Other", '')
+    ("Art Fair", 'Art Fair'), 
+    ("Air Play", 'Air Play'),
+    ("Art Parade", 'Art Parade'),
+    ("ArtWalk", 'ArtWalk'),
+    ("Cabaret FestivAll", 'Cabaret FestivAll'),
+    ("Carriage Trail", 'Carriage Trail'),
+    ("Chili Cookoff", 'Chili Cookoff'),
+    ("Civic Chorus", 'Civic Chorus'),
+    ("CLOG", 'CLOG'),
+    ("CYAC", 'CYAC'),
+    ("Dance Gala", 'Dance Gala'),
+    ("Drum Circle", 'Drum Circle'),
+    ("Festiv-ALT", 'Festiv-ALT'),
+    ("Go to Hale", 'Go to Hale'),
+    ("Ice Cream Social", 'Ice Cream Social'),
+    ("Juneteenth", 'Juneteenth'),
+    ("Live on the Levee", 'Live on the Levee'),
+    ("Mayor's Concert", "Mayor's Concert"),
+    ("Mountain Stage", 'Mountain Stage'),
+    ("Taste", 'Taste'),
+    ("Wine & All That Jazz", 'Wine & All That Jazz'),
+    ("Woofstock & Wieners", 'Woofstock & Wieners'),
+    ("Other", 'Other')
 ]
 
 marketing_sources_list = [
-    ("WV Gazette Mail", ''),
-    ("Charleston Radio", ''),
-    ("WV Public Broadcasting", ''),
-    ("WSAZ", ''),
-    ("State Journal", ''),
-    ("WTSQ Radio", ''),
-    ("WNKU Radio", ''),
-    ("WOUB Radio", ''),
-    ("WV State Visitor's Guide", ''),
-    ("Herald Tri-State Visitors Guide", ''),
-    ("WV Living Magazine", ''),
-    ("South Hills Living Magazine", ''),
-    ("FestivALL Print Brochure/Schedule", ''),
-    ("FestivALL Website", ''),
-    ("Facebook", ''),
-    ("Twitter", ''),
-    ("Instagram", ''),
-    ("Friends/Family", ''),
-    ("Other", '')
+    ("WV Gazette Mail", 'WV Gazette Mail'),
+    ("Charleston Radio", 'Charleston Radio'),
+    ("WV Public Broadcasting", 'WV Public Broadcasting'),
+    ("WSAZ", 'WSAZ'),
+    ("State Journal", 'State Journal'),
+    ("WTSQ Radio", 'WTSQ Radio'),
+    ("WNKU Radio", 'WNKU Radio'),
+    ("WOUB Radio", 'WOUB Radio'),
+    ("WV State Visitor's Guide", "WV State Visitor's Guide"),
+    ("Herald Tri-State Visitors Guide", 'Herald Tri-State Visitors Guide'),
+    ("WV Living Magazine", 'WV Living Magazine'),
+    ("South Hills Living Magazine", 'South Hills Living Magazine'),
+    ("FestivALL Print Brochure/Schedule", 'FestivALL Print Brochure/Schedule'),
+    ("FestivALL Website", 'FestivALL Website'),
+    ("Facebook", 'Facebook'),
+    ("Twitter", 'Twitter'),
+    ("Instagram", 'Instagram'),
+    ("Friends/Family", 'Friends/Family'),
+    ("Other", 'Other')
 ]
     
 accomodations_list = [
-    ("I am staying with family or friends", ''),
-    ("I am staying in paid lodging", '')
+    ("I am staying with family or friends", 'I am staying with family or friends'),
+    ("I am staying in paid lodging", 'I am staying in paid lodging')
 ]
 
 education_list = [
-    ("High School", ''),
-    ("Some College", ''),
-    ("College Graduate", ''),
-    ("Graduate Degree", '')
+    ("High School", 'High School'),
+    ("Some College", 'Some College'),
+    ("College Graduate", 'College Graduate'),
+    ("Graduate Degree", 'Graduate Degree')
 ]
 
 age_list = [
-    ("Pre-teen / teen", ''),
-    ("20-29", ''),
-    ("30-39", ''),
-    ("40-49", ''),
-    ("50-59", ''),
-    ("60-69", ''),
-    ("70+", '')
+    ("Pre-teen / teen", 'Pre-teen / teen'),
+    ("20-29", '20-29'),
+    ("30-39", '30-39'),
+    ("40-49", '40-49'),
+    ("50-59", '50-59'),
+    ("60-69", '60-69'),
+    ("70+", '70+')
 ]
 
+income_list = [
+    ("less than $30,000", "less than $30,000"),
+    ("$30,000 - $49,999", "$30,000 - $49,999"),
+    ("$50,000 - $69,999", "$50,000 - $69,999"),
+    ("$70,000 - $99,999", "$50,000 - $69,999"),
+    ("more than $100,000", "$50,000 - $69,999")
+]
+		
 class Survey(FlaskForm):
-    #StringField('name', validators=[DataRequired()])
     event_name = RadioField('Name of Event', choices=event_list)
-    home_zip = IntegerField("What is your home zip code?", validators=[Length(5)])
+    home_zip = StringField("What is your home zip code?", validators=[Length(5)])
     visit_accomodations = RadioField("If you are visiting:", choices=accomodations_list)
     nights_staying =  IntegerField("How many nights are you staying?")
     festivall_attendance = BooleanField("Have you attended FestivALL in the past?")
@@ -84,6 +90,7 @@ class Survey(FlaskForm):
     money_spent = DecimalField("Not counting lodging, approximately how much will you spend here during FestivALL?")
     age = RadioField("What is your age?", choices=age_list)
     education = RadioField("What is your education level?", choices=education_list)
+    income = RadioField("What is your household income level?", choices = income_list)
     festivall_marketing = SelectField("Where did you hear about FestivALL? (check all that apply)", choices=marketing_sources_list)
     festivall_fav = StringField("What do you like best about FestivALL?")
     festivall_suggestion = StringField("What would you like to see at FestivALL?")
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, request
 from . import app
 from .spreadsheet import *
 from .forms import *
@@ -10,10 +10,27 @@ def index():
 @app.route("/survey")
 def survey():
     form = Survey()
-    if form.validate_on_submit():
-        return redirect('/submitted')
     return render_template('survey.html', form=form)
 
-@app.route('/submitted')
+@app.route('/submit', methods=['POST'])
 def submit():
+    print(request.form)
+    insert_survey([request.form['event_name'],
+        request.form['home_zip'],
+        request.form['visit_accomodations'],
+        request.form['nights_staying'],
+        request.form['festivall_attendance'],
+        request.form['num_events_to_attend'],
+        request.form['money_spent'],
+        request.form['age'],
+        request.form['education'],
+        request.form['income'],
+        request.form['festivall_marketing'],
+        request.form['festivall_fav'],
+        request.form['festivall_suggestion'],
+        request.form['email']])
+    return redirect('/submitted')
+    
+@app.route('/submitted')
+def submitted():
     return "Survey submitted! <a href='/survey'>Submit another?</a>"
